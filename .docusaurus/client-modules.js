a880a414c54740f5acd3ed209609893dd4873719
Ryan <30853424+totally-not-frito-lays@users.noreply.github.com>
2025-01-30 11:37:24 -0800

    init sveltekit w/ tailwind

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/home/rlay/Documents/0-Projects/totally-not-frito-lays.github.io/node_modules/infima/dist/css/default/default.css"),
+  require("/home/rlay/Documents/0-Projects/totally-not-frito-lays.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/rlay/Documents/0-Projects/totally-not-frito-lays.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/rlay/Documents/0-Projects/totally-not-frito-lays.github.io/src/css/custom.css"),
+];
